c604447d41afbcd8c3a52b47a4e8ee87d5be203a
mohits-git <mohitkhatri2805@gmail.com>
2025-07-16 22:20:12 +0530

    ubuntu

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ echo "Warning: You need neovim v10+ installed for the nvim config to work proper
 echo ""
 echo "Warning: If you have any of the following dotfiles, rename them them to <file_name/folder_name>.backup or something else to avoid conflicts, before running this script"
 echo "zshrc: ~/.zshrc"
-echo "ghostty: ~/.config/ghostty/config"
 echo "tmux.conf: ~/.tmux.conf"
 echo "nvim: ~/.config/nvim"
 echo "gitconfig: ~/.gitconfig"
@@ -22,7 +21,6 @@ fi
 
 echo "Setting up dotfiles symlinks"
 stow zshrc
-stow ghostty
 stow tmux.conf
 stow nvim
 stow gitconfig 
